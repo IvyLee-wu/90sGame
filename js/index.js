e919700e8d1b7caca6e7372ed5913e4fe5115c5d
IvyLee-wu <45351785+IvyLee-wu@users.noreply.github.com>
2019-08-19 01:23:10 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,32 +1,20 @@
 const getRandom = (max, min) =>{
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-const keyFrame = (self) => {
+/*const keyFrame = (self) => {
     self.anims.create({
         key: 'run',
         frames: self.anims.generateFrameNumbers('user', { start: 0, end: 3 }),
         frameRate: 5,
         repeat: -1
     })
-    /*self.anims.create({
-        key: 'jump',
-        frames: self.anims.generateFrameNumbers('user', { start: 2, end: 3 }),
-        frameRate: 5,
-        repeat: -1
-    })
-    self.anims.create({
-        key: 'speed',
-        frames: self.anims.generateFrameNumbers('user', { start: 4, end: 5 }),
-        frameRate: 5,
-        repeat: -1
-    })*/
     self.anims.create({
         key: 'stop',
         frames: self.anims.generateFrameNumbers('user', { start: 4, end: 5 }),
         frameRate: 5,
         repeat: -1
     })
-}
+}*/
 
 const gameStart = {
     key: 'gameStart',
